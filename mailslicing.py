4120b321f12af6c6b4922b13ee70b3e571cd6623
Muhammed Ramadan <muhammedramadan870@gmail.com>
2022-12-27 17:36:51 +0200

    firstcommit

---
+++ mailslicing.py
@@ -0,0 +1,24 @@
+# -------------------------------
+# -- Slice Email --
+# -------------------------------
+
+theName = input('what\'s your name?').strip().capitalize()
+theEmail = input('what\'s your email?').strip()
+
+theUsername = theEmail[0:theEmail.index("@")]
+theDomain = theEmail[theEmail.index("@") + 1:]
+
+
+print(f"Hello {theName} your Email is {theEmail}")
+print(f"your username is {theUsername} \n and your website is {theDomain}")
+
+
+
+
+
+
+
+
+#email = "muhammedramadan870@gmail.com"
+#print(email[0:email.index("@")])
+#print(email.index("@"))
